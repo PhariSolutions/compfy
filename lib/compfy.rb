87b41e1dcb41ea5f607225ca6002dc2e06b0e2ea
Alan Bispo <alan@phari.solutions>
2018-03-08 22:28:21 -0300

    Configuração inicial da Gem

---
+++ lib/compfy.rb
@@ -0,0 +1,5 @@
+require "compfy/version"
+
+module Compfy
+  # Your code goes here...
+end
---
+++ spec/compfy_spec.rb
@@ -0,0 +1,9 @@
+RSpec.describe Compfy do
+  it "has a version number" do
+    expect(Compfy::VERSION).not_to be nil
+  end
+
+  it "does something useful" do
+    expect(false).to eq(true)
+  end
+end
